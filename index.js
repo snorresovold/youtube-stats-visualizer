61b3b3797f8cac6b99e12c8055f11c0f74d8054f
snorresovold <snorresovold@gmail.com>
2022-05-18 21:58:41 +0200

    made it automatically add js and stuff

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const subCount = document.getElementById('subcount');
 
 const viewCount = document.getElementById('viewcount');
 
-let getSubscribers = () => {
+const getSubscribers = () => {
 
     fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
     .then(response => {
@@ -15,9 +15,17 @@ let getSubscribers = () => {
     .then(data => {
         subCount.innerHTML = data["items"][0].statistics.subscriberCount;
         viewCount.innerHTML = data["items"][0].statistics.viewCount;
-        console.log(data["items"][0].statistics)
-    })
+        // console.log(data["items"][0].statistics)
+        for(let i = 0; i <= data["items"][0].statistics.subscriberCount; i++){
 
+            let tag = document.createElement("p")
+            let text = document.createTextNode(i)
+            tag.appendChild(text)
+            let element = document.getElementById("yt-1")
+            element.prepend(tag)
+            
+        }
+    })
 }
 
 getSubscribers();
